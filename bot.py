b961c25f840bb247aa97099d25ebe856157f0d08
Nona1802 <149075714+Nona1802@users.noreply.github.com>
2024-10-08 21:21:58 +0400

    First commit

---
+++ bot.py
@@ -0,0 +1,290 @@
+my_token = '7447463411:AAEH7FzFJT0vx7NCwBH_ruvWp83MjUcw2hQ'
+
+idol_photos = {
+    "BTS": "https://www.hyundai.com/content/dam/hyundai/ww/en/images/worldcup/08-bts/worldcup-bts-stg-m.jpg",
+    "Monsta X": "https://0.soompi.io/wp-content/uploads/2019/01/06051704/monsta-x6.jpg",
+    "Seventeen": "https://representasianproject.com/wp-content/uploads/2023/11/seventeen-header.jpg",
+    "The Rose": "https://pm1.aminoapps.com/6941/88b908d33b313f9e25256b72a5c592f3279ed20ar1-1000-600v2_uhq.jpg"
+}
+
+idol_info = {
+    "BTS": "BTS, also known as Bangtan Boys, is a seven-member South Korean boy band formed by Big Hit Entertainment in 2013. There are 7 members in BTS: RM, Jin, Suga, J-Hope, Jimin, Taehyung, Jungkook.",
+    "Monsta X": "Monsta X is a South Korean boy group formed through a survival show by Starship Entertainment in 2015. There are 6 present and 1 former members in Monsta X: Shownu, Kihyun, Minhyuk, Jooheon, Hyungwon, Changkyun and Wonho who left the group.",
+    "Seventeen": "Seventeen is a South Korean boy group formed by Pledis Entertainment in 2015. There are 13 members in Seventeen: Jeonghan, Vernon, Jun, Mingyu, Hoshi, Joshua, The8, Woozi, Dino, Wonwoo, Seungkwan, DK, S.Coups.",
+    "The Rose": "The Rose is a South Korean rock band formed by J&Star Company in 2017, known for their unique sound and powerful vocals. There are 4 members in The Rose: Wosung, Dojoon, Hajoon, Jaeheong."
+}
+
+member_info = {
+    "BTS": {
+        "RM": {
+            "info" : "Kim Namjoon, better known as RM, is the leader of BTS and one of the main rappers.",
+            "photo" : "https://www.allkpop.com/upload/2023/03/content/131532/web_data/allkpop_1678735928_allkpop-1678726292-untitled-1.jpg"},
+        "Jin": {"info" : "Kim Seokjin, also known as Jin, is a vocalist and the oldest member of BTS.",
+                "photo" : "https://wwd.com/wp-content/uploads/2024/08/09_2697_M1.jpg?crop=0px%2C181px%2C1998px%2C1121px&resize=1000%2C563"},
+        "Suga": {"info" : "Min Yoongi, known as Suga, is a rapper, songwriter, and producer in BTS.",
+                 "photo" : "https://people.com/thmb/z6ES4mpAaDMpHKku_LiXfzn8w5Q=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(737x253:739x255)/suga-bts-military-091823-tout-eb23d4e5a72e403fb06d0a8d4b688bfd.jpg"},
+        "J-Hope": {"info" : "Jung Hoseok, known as J-Hope, is the sub rapper, sub vocalist and main dancer of BTS.",
+                   "photo" : "https://www.esquireme.com/cloud/2023/02/24/j-hope-683x1024.jpg"},
+        "Jimin": {"info" : "Park Jimin, known as Jimin, is a lead vocalist and dancer in BTS.",
+                  "photo" : "https://culted.com/wp-content/uploads/2023/10/Screenshot-2023-10-09-at-16.19.56.jpg"},
+        "V": {"info" : "Kim Taehyung, known as V, is a vocalist and visual of BTS.",
+              "photo" : "https://www.rappler.com/tachyon/2023/08/bts-v-solo-album.png"},
+        "Jungkook": {"info" : "Jeon Jungkook, known as Jungkook, is the main vocalist and youngest member of BTS.",
+                     "photo" : "https://ew.com/thmb/BjUrPQmw5dJl5A2TD9BLfwAEcYE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/Jungkook-6837e9da24d24d428979a41d6df11012.jpg"}
+    },
+    "Monsta X": {
+        "Shownu": {"info" : "Shownu is the leader and one of the vocalists in Monsta X.",
+                   "photo" : "https://kpopping.com/documents/27/0/230727-Monsta-X-Shownu-THE-UNSEEN-Promotional-Photoshoot-with-OSEN-documents-4.jpeg?v=05569"},
+        "Kihyun": {"info" : "Kihyun is the main vocalist in Monsta X.",
+                   "photo" : "https://thebiaslist.com/wp-content/uploads/2022/10/kihyun-monsta-x-youth.jpeg"},
+        "Minhyuk": {"info" : "Minhyuk is a vocalist and visual in Monsta X.",
+                    "photo" : "https://www.billboard.com/wp-content/uploads/2023/03/minhyuk-monsta-x-press-cr-starship-entertainment-2023-billboard-1548.jpg"},
+        "Wonho (former)": {"info" : "Wonho was a vocalist and visual in Monsta X.",
+                  "photo" : "https://i0.wp.com/kstationtv.com/wp-content/uploads/2017/11/Monsta-x-Wonho-1.jpg?fit=960%2C640&ssl=1"},
+        "Jooheon": {"info" : "Jooheon is the main rapper in Monsta X.",
+                    "photo" : "https://static.asiachan.com/full/36/21/251086.jpg"},
+        "Hyungwon": {"info" : "Hyungwon is a vocalist and dancer in Monsta X.",
+                     "photo" : "https://upload.wikimedia.org/wikipedia/commons/4/4a/HYUNGWON_MarieClarieKorea_2021.png"},
+        "I.M": {"info" : "I.M is the lead rapper and maknae in Monsta X.",
+                "photo" : "https://www.nme.com/wp-content/uploads/2022/11/monsta-x-i-m-sign-sony-music-korea-solo-artist.jpg"}
+    },
+    "Seventeen": {
+        "S.Coups": {"info" : "S.Coups is the leader and rapper of Seventeen.",
+                    "photo" : "https://akcdn.detik.net.id/visual/2023/08/08/scoups-seventeen_43.jpeg?w=360&q=90"},
+        "Jeonghan": {"info" : "Jeonghan is a lead vocalist in Seventeen.",
+                     "photo" : "https://www.masala.com/cloud/2024/09/12/1726113862517JEONGHAN.jpg"},
+        "Joshua" : {"info" : "Joshua is a lead vocalist and visual in Seventeen.",
+                    "photo" : "https://i.pinimg.com/736x/24/0d/2a/240d2ae1b5f88e2f2e8348eb7ac04495.jpg"}, 
+        "Jun" : {"info" : "Jun is a lead dancer and sub-vocalist in Seventeen.",
+                 "photo" : "https://en.kepoper.com/wp-content/uploads/2021/03/jun-profile-1.jpg"}, 
+        "Hoshi" : {"info" : "Hoshi is the Performance Team Leader, main dancer, lead vocalist and sub-rapper of Seventeen.",
+                   "photo" : "https://en.kepoper.com/wp-content/uploads/2021/03/hoshi-profile-1.jpg"}, 
+        "Wonwoo" : {"info" : "Wonwoo is a rapper and sub-vocalist in Seventeen.",
+                    "photo" : "https://static.wikia.nocookie.net/girlgroup/images/3/31/Wonwoo_17_Is_Right_Here_Hear.zip_Ver_1.jpg/revision/latest/scale-to-width-down/1200?cb=20240502082916"},
+        "Woozi" : {"info" : "Woozi is the Vocal Team Leader, Lead Vocalist, Producer of Seventeen.",
+                   "photo" : "https://i.ebayimg.com/images/g/4~cAAOSwc5hjYfGC/s-l1200.jpg"},
+        "DK" : {"info" : "DK is a main vocalist in Seventeen.",
+                "photo" : "https://dbkpop.com/wp-content/uploads/2019/08/seventeen_hit_concept_DK.jpg"},
+        "Mingyu" : {"info" : "Mingyu is a rapper, sub-vocalist, visual and Face of the Group in Seventeen. He is the most popular member in Seventeen.",
+                    "photo" : "https://www.billboard.com/wp-content/uploads/2022/08/Mingyu-of-SEVENTEEN-2021-billboard-1548.jpg"},
+        "The8" : {"info" : "The8 is a lead dancer, sub-vocalist, sub-rapper in Seventeen.",
+                  "photo" : "https://dbkpop.com/wp-content/uploads/2019/08/seventeen_hit_concept_the8.jpg"}, 
+        "Seungkwan" : {"info" : "Seungkwan is a main vocalist and Face of the Group in Seventeen.",
+                       "photo" : "https://kpopping.com/documents/f9/2/2000/batch_LIV_3887-copy.jpeg?v=ba791"}, 
+        "Vernon" : {"info" : "Vernon is a rapper, sub-vocalist, visual and Face of the Group in Seventeen.",
+                    "photo" : "https://0.soompi.io/wp-content/uploads/2022/03/03213909/seventeen-vernon-6.jpg"}, 
+        "Dino" : {"info" : "Dino is a main dancer, sub-vocalist, sub-rapper and maknae in Seventeen.",
+                  "photo" : "https://pbs.twimg.com/media/EbLknAzU4AAKhPT.jpg"}
+    },
+    "The Rose": {
+        "Woosung": {"info" : "Woosung is the leader, guitarist and vocalist of The Rose. Also he is succesfull soloist. He is the most popular member in The Rose.",
+                    "photo" : "https://i.pinimg.com/736x/5e/d0/97/5ed0975de1cc21d9b9ed947bd5bed4f9.jpg"},
+        "Dojoon": {"info" : "Dojoon is a vocalist and guitarist in The Rose.",
+                   "photo" : "https://i.pinimg.com/736x/a7/7c/04/a77c0424c7450c308983eb07189efb67.jpg"},
+        "Hajoon": {"info" : "Hajoon is the drummer and sub-vocalist in The Rose.",
+                   "photo" : "https://i0.wp.com/kpoplivepolska.pl/wp-content/uploads/2022/08/The-Rose-Ha-Joon.jpg?fit=1200%2C800&ssl=1"},
+        "Jaehyeong": {"info" : "Jaehyeong is the bassist and sub-vocalist in The Rose.",
+                      "photo" : "https://i.pinimg.com/736x/0c/f9/03/0cf90384a5a1c160c53a2c194ce5494f.jpg"}
+    }
+}
+
+idol_songs = {
+    "BTS": {
+        "Dynamite": "https://www.youtube.com/watch?v=gdZLi9oWNZg",
+        "Butter": "https://www.youtube.com/watch?v=WMweEpGlu_U"
+    },
+    "Monsta X": {
+        "Gambler": "https://www.youtube.com/watch?v=yY13X0BKaUw",
+        "One Day": "https://www.youtube.com/watch?v=U4_pc_Gqrvk"
+    },
+    "Seventeen": {
+        "Don't Wanna Cry": "https://www.youtube.com/watch?v=9M7k9ZV67c0",
+        "Super": "https://www.youtube.com/watch?v=-GQg25oP0S4"
+    },
+    "The Rose": {
+        "Back To Me": "https://www.youtube.com/watch?v=fuzLp9gDcwY",
+        "You're beautiful": "https://www.youtube.com/watch?v=7IFJQRlcNQ0"
+    }
+}
+
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, ContextTypes, CommandHandler, CallbackQueryHandler, MessageHandler, filters
+import yt_dlp
+import os
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute()
+    conn.commit()
+    conn.close()
+
+def log_user(user_id, username):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT OR IGNORE INTO users (user_id, username) VALUES (?, ?)', (user_id, username))
+    conn.commit()
+    conn.close()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = update.effective_user.id
+    username = update.effective_user.username
+
+    log_user(user_id, username) 
+    await update.message.reply_text(f"Hello {update.effective_user.first_name}! I'm a bot for introducing K-pop idols.")
+    await update.message.reply_text('Send /help to continue.')
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_id = update.effective_user.id
+    username = update.effective_user.username
+
+    log_user(user_id, username)  
+    keyboard = [
+        [InlineKeyboardButton("BTS", callback_data="BTS")],
+        [InlineKeyboardButton("Monsta X", callback_data="Monsta X")],
+        [InlineKeyboardButton("Seventeen", callback_data="Seventeen")],
+        [InlineKeyboardButton("The Rose", callback_data="The Rose")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text(f"{update.effective_user.first_name}, which K-pop idol group are you interested in?", reply_markup=reply_markup)
+
+def download_youtube_video(youtube_url, output_dir="downloads"):
+    try:
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': os.path.join(output_dir, '%(title)s.%(ext)s'),
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'quiet': True,
+        }
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(youtube_url, download=True)
+            audio_file = ydl.prepare_filename(info_dict).replace(".webm", ".mp3").replace(".m4a", ".mp3")
+        
+        return audio_file
+
+    except Exception as e:
+        print(f"Error downloading video: {e}")
+        return None
+
+async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+
+    if query.data in idol_photos and query.data in idol_info:
+        group_name = query.data
+        photo_url = idol_photos[group_name]
+        group_description = idol_info[group_name]
+
+        await context.bot.send_photo(
+            chat_id=update.effective_chat.id,
+            photo=photo_url,
+            caption=f"{group_description}"
+        )
+
+        if group_name in member_info:
+            member_keyboard = [[InlineKeyboardButton(member, callback_data=f"member_{group_name}_{member}")] for member in member_info[group_name]]
+            member_markup = InlineKeyboardMarkup(member_keyboard)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="Are you interested in a specific member?", reply_markup=member_markup)
+
+    elif query.data.startswith("member_"):
+        _, group_name, member_name = query.data.split('_')
+        if group_name in member_info and member_name in member_info[group_name]:
+            member_details = member_info[group_name][member_name]
+
+            await context.bot.send_photo(chat_id=update.effective_chat.id, photo=member_details["photo"], caption=member_details["info"])
+
+            keyboard = [
+                [InlineKeyboardButton("Yes", callback_data=f"songs_{group_name}")],
+                [InlineKeyboardButton("No", callback_data="no_songs")]
+            ]
+            reply_markup = InlineKeyboardMarkup(keyboard)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Would you like to hear some songs from {group_name}?", reply_markup=reply_markup)
+
+    elif query.data.startswith("songs_"):
+        group_name = query.data.replace("songs_", "")
+        songs = idol_songs.get(group_name, {})
+
+        if songs:
+            song_keyboard = [[InlineKeyboardButton(f"Download {title}", callback_data=f"download_{group_name}_{title}")] for title in songs]
+            reply_markup = InlineKeyboardMarkup(song_keyboard)
+            song_list = "\n".join([f"{title}: {url}" for title, url in songs.items()])
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Here are some popular songs by {group_name}:\n{song_list}\nWould you like to download one?", reply_markup=reply_markup)
+        else:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=f"No songs available for {group_name}.")
+
+    elif query.data.startswith("download_"):
+        _, group_name, song_title = query.data.split('_')
+        song_url = idol_songs[group_name].get(song_title)
+        
+        if song_url:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Downloading {song_title}...")
+
+            downloaded_file = download_youtube_video(song_url)
+            
+            if downloaded_file:
+                with open(downloaded_file, 'rb') as f:
+                    await context.bot.send_audio(chat_id=update.effective_chat.id, audio=f, title=song_title)
+
+                os.remove(downloaded_file)
+            else:
+                await context.bot.send_message(chat_id=update.effective_chat.id, text="Failed to download the song. Please try again later.")
+        
+        keyboard = [
+            [InlineKeyboardButton("Yes", callback_data="send_link")],
+            [InlineKeyboardButton("No", callback_data="no_more_songs")]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Do you want to download another song using a YouTube link?", reply_markup=reply_markup)
+    
+    elif query.data == "send_link":
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Please send me the YouTube link of the song you want to download.")
+
+    elif query.data == "no_songs" or query.data == "no_more_songs":
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Okay! Let me know if you want to hear songs later.")
+
+async def download_song_from_link(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    youtube_url = update.message.text
+    
+    await update.message.reply_text(f"Downloading song from the provided link: {youtube_url}...")
+
+    downloaded_file = download_youtube_video(youtube_url)
+    
+    if downloaded_file:
+        song_title = os.path.splitext(os.path.basename(downloaded_file))[0]  
+
+        with open(downloaded_file, 'rb') as f:
+            await update.message.reply_audio(audio=f, title=song_title)  
+
+        os.remove(downloaded_file)
+
+        keyboard = [
+            [InlineKeyboardButton("Yes", callback_data="send_link")],
+            [InlineKeyboardButton("No", callback_data="no_more_songs")]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        await update.message.reply_text("Do you want to download another song using a YouTube link?", reply_markup=reply_markup)
+
+    else:
+        await update.message.reply_text("Failed to download the song. Please try again later.")
+
+
+if __name__ == '__main__':
+    init_db()  
+    app = Application.builder().token(my_token).build()
+
+    app.add_handler(CommandHandler('start', start))
+    app.add_handler(CommandHandler('help', help))
+    app.add_handler(CallbackQueryHandler(button_handler))
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), download_song_from_link))  
+
+    print('Bot started...')
+    app.run_polling()
